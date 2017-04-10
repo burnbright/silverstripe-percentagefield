aff847ff365fcaff019487406cbee94f018f3efd
Jeremy Shipman <jeremy@burnbright.net>
2017-04-10 20:07:29 +1200

    Added a test, composer file, gitignore

--- tests/PercentageFieldTest.php
+++ tests/PercentageFieldTest.php
@@ -1,2 +1,11 @@
 <?php
 
+class PercentageFieldTest extends SapphireTest {
+
+	public function testPercentField() {
+		$field = PercentageField::create("Percent");
+		$field->setValue(45);
+		$this->assertEquals(0.45, $field->dataValue());
+	}
+
+}
